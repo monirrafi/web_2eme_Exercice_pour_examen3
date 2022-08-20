6380623d7c1edb6666b309709beb2e0684b64091
monirrafi <programermonirrafi@gmail.com>
2022-08-20 08:55:16 -0400

    samedi_20/08/2022_08h55

--- js/fonctions.js
+++ js/fonctions.js
@@ -14,12 +14,44 @@ let chargerListesNoDoublants = () => {
 }
 chargerListesNoDoublants();
 let clickProduits = () => {
-    document.getElementById('contenu').innerHTML = lister(listeCommandes);
+    let rep =`
+
+    <table>
+    <caption>Colored Table Header</caption>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Savings</th>
+      </tr>
+      <tr>
+        <td>Peter</td>
+        <td>Griffin</td>
+        <td class="tdcenter">$100</td>
+      </tr>
+      <tr>
+        <td>Lois</td>
+        <td>Griffin</td>
+        <td class="tdcenter">$150</td>
+      </tr>
+      <tr>
+        <td>Joe</td>
+        <td>Swanson</td>
+        <td class="tdcenter">$300</td>
+      </tr>
+      <tr>
+        <td>Cleveland</td>
+        <td>Brown</td>
+        <td class="tdcenter">$250</td>
+    </tr>
+    </table>
+    
+`;
+    document.getElementById('contenu').innerHTML = rep;
 
 }
 let changerTotal = () => {
     let selTotal = document.getElementById('selTotal');
-    let choix = listeTotal[selTotal.options.selectedIndex];
+    let choix = selTotal.options[selTotal.options.selectedIndex].text;
     document.getElementById('contenu').innerHTML = calculerTotal(choix);
 }
 let calculerTotal = (no) => {
@@ -83,7 +115,8 @@ let ChargerSelect = () => {
     for(let client of listeClients){
         selClients.options[selClients.options.length]= new Option(client); 
     }
-    document.getElementById('contenu').innerHTML = lister(listeCommandes);
+    clickProduits();
+    //document.getElementById('contenu').innerHTML = lister(listeCommandes,'');
 }
 let listerUnChoix = (cle,choix) => {
     let retourTableau = [];
@@ -98,126 +131,105 @@ let listerUnChoix = (cle,choix) => {
 let changerProduit = () => {
     let selProduits = document.getElementById('selProduits');
     let choix = listeProduits[selProduits.selectedIndex];
-    document.getElementById('contenu').innerHTML = lister(listerUnChoix('Product',choix),'P');
+    document.getElementById('contenu').innerHTML = lister(listerUnChoix('Product',choix),'P',choix);
 
 }
 let changerClient = () => {
     let selClients = document.getElementById('selClients');
     let choix = listeClients[selClients.selectedIndex];
-    document.getElementById('contenu').innerHTML = lister(listerUnChoix('Customer',choix),'C');
+    document.getElementById('contenu').innerHTML = lister(listerUnChoix('Customer',choix),'C',choix);
 
 }
 
-let entete = (cle) => {
-    let retourTableau = `
-    <table class="table table-primary table-striped table-hover">
+let entete = (pour,nomDePour) => {
+    let rep = `
+        <table>
+        `;
+        if(pour=='P'){
+            rep += `
+                <caption>LISTE DES CLIENTS QU'ONT COMMANDÉ LE PRODUIT ${nomDePour}</caption>
+                <thead>
+                <tr><th>Client</th>
+            `;
+        } else if(pour=='C'){
+            rep += `
+                <caption>LISTE DES PRODUITS COMMANDÉS PAR LE CLIENT ${nomDePour}</caption>
+                <thead>
+                <tr><th>Produit</th>
+            `;
+        }else {//Tout la liste
+                rep += `
+                <caption>LISTE DE TOUTES LES COMMANDES</caption>
+                <thead>
 
-    `;
-    if(cle == 'P'){
-        retourTableau += `
-        <thead>
-          <tr>
-            <th scope="col">Clients</th>
-            <th scope="col">Total1</th>
-            <th scope="col">Total2</th>
-            <th scope="col">Total3</th>
-            <th scope="col">Total4</th>
-          </tr>
+                <tr><th>Produit</th><th>Client</th>
+                `;
+        }
+        rep+=`<th>Qtr 1</th><th>Qtr 2</th><th>Qtr 3</th><th>Qtr 4</th></tr>
         </thead>
-        <tbody>
         `;
-    
-    }else if(cle == 'C') {
-        retourTableau += `
-        <thead>
-        <tr>
-            <th scope="col">Produits</th>
-            <th scope="col">Total1</th>
-            <th scope="col">Total2</th>
-            <th scope="col">Total3</th>
-            <th scope="col">Total4</th>
-          </tr>
-        </thead>
-        <tbody>
-        `;
-    
-    }else{
-    retourTableau += `
-    <thead>
-      <tr>
-        <th scope="col">Produits</th>
-        <th scope="col">Clients</th>
-        <th scope="col">Total1</th>
-        <th scope="col">Total2</th>
-        <th scope="col">Total3</th>
-        <th scope="col">Total4</th>
-      </tr>
-    </thead>
-    <tbody>
-    `;
-    }
-    return retourTableau;
+    return rep;
 }
 
-let lister = (liste,cle) => {
-    let retourTableau = entete(cle);
+let lister = (liste,cle,choix) => {
+    let retourTableau = entete(cle,choix);
     for(let commande of liste){
         if(cle == 'P'){
             retourTableau +=`
             <tr>
-                <th scope="col">${commande.Customer}</th>
+                <th>${commande.Customer}</th>
             `;
     
         }else if(cle == 'C'){
             retourTableau +=`
             <tr>
-                <th scope="col">${commande.Product}</th>
+                <th>${commande.Product}</th>
     
             `;
     
         }else{
             retourTableau +=`
             <tr>
-                <th scope="col">${commande.Product}</th>
-                <th scope="col">${commande.Customer}</th>
+                <th>${commande.Product}</th>
+                <th>${commande.Customer}</th>
     
             `;
     
         }
         if(commande['Qtr 1'] != undefined){
             retourTableau +=`
-            <th scope="col">${commande['Qtr 1']}</th>
+            <th>${commande['Qtr 1']}</th>
             `;
         }else{    
             retourTableau +=`
-            <th scope="col"></th>
+            <th></th>
             `;
         }
         if(commande['Qtr 2'] != undefined){
             retourTableau +=`
-            <th scope="col">${commande['Qtr 2']}</th>
+            <th>${commande['Qtr 2']}</th>
             `;
         }else{    
             retourTableau +=`
-            <th scope="col"></th>
+            <th></th>
             `;
         }
         if(commande['Qtr 3'] != undefined){
             retourTableau +=`
-            <th scope="col">${commande['Qtr 3']}</th>
+            <th>${commande['Qtr 3']}</th>
             `;
         }else{    
             retourTableau +=`
-            <th scope="col"></th>
+            <th></th>
             `;
         }
         if(commande['Qtr 4'] != undefined){
             retourTableau +=`
-            <th scope="col">${commande['Qtr 4']}</th>
+            <th>${commande['Qtr 4']}</th>
             `;
         }else{    
             retourTableau +=`
-            <th scope="col"></th>
+            <th></th>
             `;
         }
    
